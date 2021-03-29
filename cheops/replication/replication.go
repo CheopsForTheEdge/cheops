406c1cfeebf445951f4f56a96eb445758e844301
Marie-Donnie <delavergnemarie@gmail.com>
2021-03-29 17:32:10 +0200

    Corrected attribute visibility

--- cheops/replication/replication.go
+++ cheops/replication/replication.go
@@ -9,13 +9,13 @@ import (
 )
 
 type Replica struct {
-	site 	string `json:"site"`
+	Site 	string `json:"site"`
 	ID 		string `json:"ID"`
 }
 
 type Replicant struct {
-	metaID      string    `json:"ID"`
-	replicas	[]Replica `json:"replicas"`
+	MetaID      string    `json:"ID"`
+	Replicas	[]Replica `json:"replicas"`
 }
 
 type allReplicants []Replicant
@@ -25,10 +25,10 @@ type allReplicants []Replicant
 
 var Replicants = allReplicants{
 	{
-		metaID:      "33344596",
-		replicas:     []Replica{
-			Replica{site: "Paris", ID: "65"},
-			Replica{site: "Nantes", ID: "42"},
+		MetaID:      "33344596",
+		Replicas:     []Replica{
+			Replica{Site: "Paris", ID: "65"},
+			Replica{Site: "Nantes", ID: "42"},
 			},
 	},
 }
@@ -53,7 +53,7 @@ func GetReplicant(w http.ResponseWriter, r *http.Request) {
 	metaID := mux.Vars(r)["metaID"]
 
 	for _, rep := range Replicants {
-		if rep.metaID == metaID {
+		if rep.MetaID == metaID {
 			json.NewEncoder(w).Encode(rep)
 			return
 		}
@@ -79,12 +79,12 @@ func AddReplica(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &updatedReplicant)
 
 	for i, rep := range Replicants {
-		if rep.metaID == metaID {
+		if rep.MetaID == metaID {
 			var replica Replica
-			var numberReplicas = len(rep.replicas)
-			replica.site = updatedReplicant.replicas[numberReplicas].site
-			replica.ID = updatedReplicant.replicas[numberReplicas].site
-			rep.replicas = append(rep.replicas, replica)
+			var numberReplicas = len(rep.Replicas)
+			replica.Site = updatedReplicant.Replicas[numberReplicas].Site
+			replica.ID = updatedReplicant.Replicas[numberReplicas].Site
+			rep.Replicas = append(rep.Replicas, replica)
 			Replicants = append(Replicants[:i], rep)
 			json.NewEncoder(w).Encode(rep)
 		}
@@ -95,7 +95,7 @@ func AddReplica(w http.ResponseWriter, r *http.Request) {
 func DeleteReplicant(w http.ResponseWriter, r *http.Request) {
 	metaID := mux.Vars(r)["metaID"]
 	for i, rep := range Replicants {
-		if rep.metaID == metaID {
+		if rep.MetaID == metaID {
 			Replicants = append(Replicants[:i], Replicants[i+1:]...)
 			fmt.Fprintf(w,
 				"The event with ID %v has been deleted successfully",
