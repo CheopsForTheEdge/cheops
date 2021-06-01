1f537bcd079cca30d3d9729f7f3e053c8aa77b84
SZYMANEK Arnaud <arnaud.szymanek@inria.fr>
2021-06-01 14:55:51 +0000

    Update scope.go

--- cheops/request/scope.go
+++ cheops/request/scope.go
@@ -21,6 +21,7 @@ func ExtractScope(w http.ResponseWriter, req *http.Request) {
 	Myscope.Sites= append(Myscope.Sites, scopes)
 
 	////Test to see if forward is working
+
 	bodyBytes, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -30,6 +31,50 @@ func ExtractScope(w http.ResponseWriter, req *http.Request) {
 
 	json.NewEncoder(w).Encode(Myscope)
 	fmt.Fprint(w, bodyString)
+	fmt.Fprint(w, req.Header)
+
+
+}
+
+func RedirectRequest (w http.ResponseWriter, req *http.Request) {
+	//Check if the incoming body is nil or not
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	client := http.Client{}
+
+	// create a new url with the good scope
+	ScopeAddr := "http://localhost:8080/scope"
+	url := fmt.Sprintf("%s", ScopeAddr)
+	proxyReq, err := http.NewRequest("GET", url, bytes.NewReader(body))
+	proxyReq.Header = req.Header
+
+	if err  != nil{
+		log.Fatal(err)
+	}
+
+	//Do the Request to the good service
+	resp , err := client.Do(proxyReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	//Test to see if forward is working
+
+	bodyt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	bodyString := string(bodyt)
+	fmt.Fprint(w, resp.Header.Get("x-request-id"))
+	fmt.Fprint(w, bodyString)
+
 
 
 }
@@ -71,7 +116,6 @@ func TestAppC (w http.ResponseWriter, req *http.Request) {
 
 
 }
-
 func TestR (w http.ResponseWriter, req *http.Request) {
 
 	bodyBytes, err := ioutil.ReadAll(req.Body)
@@ -85,52 +129,44 @@ func TestR (w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(req.Header)
 	json.NewEncoder(w).Encode(bodyString)
 
+	json.NewEncoder(w).Encode(GetAddr("10.244.4.3", "app-b"))
+
 
 
 }
 
-func RedirectRequest (w http.ResponseWriter, req *http.Request) {
-	//Check if the incoming body is nil or not
+
+func GetAddr(consulip string, servicename string) string {
+
+
+	var w http.ResponseWriter
+	var req *http.Request
+	client := http.Client{}
+
+
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return ""
 	}
-
-	client := http.Client{}
-
-	// create a new url with the good scope
-	ScopeAddr := "http://localhost:8080/scope"
-	url := fmt.Sprintf("%s", ScopeAddr)
-	proxyReq, err := http.NewRequest("GET", url, bytes.NewReader(body))
-	proxyReq.Header = req.Header
-
-	if err  != nil{
-		log.Fatal(err)
-	}
-
-	//Do the Request to the good service
-	resp , err := client.Do(proxyReq)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	//Test to see if forward is working
-	bodyt, err := ioutil.ReadAll(resp.Body)
+	url := fmt.Sprintf("%s:8500/v1/catalog/service/%s", consulip, servicename)
+	curlReq, err := http.NewRequest("GET", url,bytes.NewReader(body))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return ""
 	}
-	bodyString := string(bodyt)
-	fmt.Fprint(w, resp.Header.Get("x-request-id"))
-	fmt.Fprint(w, bodyString)
 
+	resp , err := client.Do(curlReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 
-
-}
-
-func GetScopeAddr() {
-
+	bodyresp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return ""
+	}
+	bodyString := string(bodyresp)
+	return bodyString
 }
