6dbe8bdd8116facc9791447893bb6c79490a9073
JUZDZEWSKI Matthieu <matthieu.juzdzewski@inria.fr>
2021-06-02 20:24:02 +0000

    Update scope.go

--- cheops/request/scope.go
+++ cheops/request/scope.go
@@ -7,10 +7,29 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Scope struct {
 	Sites []string `json:sites`
+}
+
+//the IP 10.244.3.3 was the consul-server-0 IP at the time of this test, it needs to be changed according to the current setup
+// ## NOTE : for now this function is just printing the IP address of a service called "app-b" by consulting the consul catalog.
+// To conclude the POCpoc, the final step is to forward the source request "r" to the IP
+func Appb (w http.ResponseWriter, r *http.Request) {
+        url := "http://10.244.3.3:8500/v1/catalog/service/app-b"
+        req, _ := http.NewRequest("GET", url, nil)
+        req.Header.Add("User-Agent", "curl/7.64.0")
+        req.Header.Add("Accept", "*/*")
+        client := &http.Client{}
+        res, _ := client.Do(req)
+        body, _ := ioutil.ReadAll(res.Body)
+        str := string(body)
+        start := strings.Index(str, "ServiceAddress") + 17
+        slice := str[start : ]
+        end := strings.Index(slice, "\"") + start
+        json.NewEncoder(w).Encode(str[start : end])
 }
 
 func ExtractScope(w http.ResponseWriter, req *http.Request) {
