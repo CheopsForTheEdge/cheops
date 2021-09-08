f89a3e59030269b56f26f972c3a20c20741641c6
root <root@econome-5.nantes.grid5000.fr>
2021-09-08 23:44:52 +0200

    openstack glue

---
+++ glue/openstack/fetch.go
@@ -0,0 +1,46 @@
+package openstack
+
+import (
+    "fmt"
+    "os/exec"
+)
+
+func Get() string {
+    app := "openstack"
+
+    arg0 := "image"
+    arg1 := "show"
+    arg2 := "testimage"
+    //arg3 := "golang"
+
+    cmd := exec.Command(app, arg0, arg1)//, arg2, arg3)
+    stdout1, err := cmd.Output()
+
+    if err != nil {
+        fmt.Println(err.Error())
+	return ""
+    }
+
+    // Print the output
+    return(string(stdout1))
+}
+
+func Deploy() string {
+    app := "openstack"
+
+    arg0 := "image"
+    arg1 := "create"
+    arg2 := "testimage"
+    //arg3 := "golang"
+
+    cmd := exec.Command(app, arg0, arg1, arg2)//, arg3)
+    stdout1, err := cmd.Output()
+
+    if err != nil {
+	    return err.Error()
+    }
+
+    // Print the output
+    return(string(stdout1))
+
+}
