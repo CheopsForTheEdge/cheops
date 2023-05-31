e93c4a38d596428c0fcdf18e56ef6f8e48b89719
Matthieu Rakotojaona <m@rako.space>
2023-05-31 14:48:59 +0200

    Proxy all operations to kube proxy

--- api/api.go
+++ api/api.go
@@ -2,12 +2,8 @@
 package api
 
 import (
-	cli "cheops.com/client"
-	"cheops.com/endpoint"
-	"cheops.com/operation"
-	"cheops.com/request"
+	"cheops.com/kubernetes"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,46 +15,50 @@ It adds handlers and routes to the router. Finally,
 it runs the listening on defined port with those routes.
 */
 func Routing() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	//commonHandlers := alice.New(CheckRequestFilledHandler)
-	// Replication
-	// router.Handle("/replication", commonHandlers.ThenFunc(operation.CreateLeaderFromOperationAPI)).Methods("POST")
-	// router.HandleFunc("/replicationLeader",
-	//operation.CreateLeaderFromOperationAPI).Methods("POST")
-	router.HandleFunc("/replication", operation.CreateReplicantAPI).Methods(
-		"POST")
-	router.HandleFunc("/replicant/{metaID}", operation.GetReplicantAPI).Methods("GET")
-	router.HandleFunc("/replicant/{metaID}", operation.AddReplica).Methods("PUT")
-	router.HandleFunc("/replicant/{metaID}", operation.DeleteReplicant).Methods("DELETE")
-	//router.Handle("/replicants", commonHandlers.ThenFunc(operation.GetAllReplicantsAPI)).Methods("GET")
-	// Endpoint
-	router.HandleFunc("/endpoint", endpoint.CreateEndpointAPI).Methods("POST")
-	router.HandleFunc("/endpoint/createsite/{Site}/{Address}", endpoint.CreateSiteAPI).Methods("POST")
-	router.HandleFunc("/endpoint/getaddress/{Site}",
-		endpoint.GetSiteAddressAPI).Methods("GET")
-	// Database
-	// Operation
-	router.HandleFunc("/operation", operation.CreateOperationAPI).Methods("POST")
-	router.HandleFunc("/operation/execute", operation.ExecuteOperationAPI).Methods("POST")
-	router.HandleFunc("/operation/localrequest",
-		operation.ExecRequestLocallyAPI).
-		Methods("POST")
-	// Broker - Driver
-	router.HandleFunc("/scope", request.ExtractScope).Methods("GET")
-	router.HandleFunc("/scope/forward", request.RedirectRequest).Methods("POST")
-	router.HandleFunc("/Appb/{flexible:.*}", request.Appb).Methods("GET")
-	router.HandleFunc("/SendRemote", request.SendRemote).Methods("GET")
-	router.HandleFunc("/RegisterRemoteSite", request.RegisterRemoteSite).Methods("POST")
-	router.HandleFunc("/GetRemoteSite/{site}", request.GetRemoteSite).Methods("GET")
-	// Client
-	router.HandleFunc("/get", cli.GetHandler)
-	router.HandleFunc("/deploy", cli.DeployHandler)
-	// router.HandleFunc("/cross/", cli.CrossHandler)
-	// router.HandleFunc("/replica/", cli.ReplicaHandler)
-	router.HandleFunc("/sendoperation", cli.SendOperationToSites)
+	/*
+		router := mux.NewRouter().StrictSlash(true)
+		router.HandleFunc("/", homeLink)
+		//commonHandlers := alice.New(CheckRequestFilledHandler)
+		// Replication
+		// router.Handle("/replication", commonHandlers.ThenFunc(operation.CreateLeaderFromOperationAPI)).Methods("POST")
+		// router.HandleFunc("/replicationLeader",
+		//operation.CreateLeaderFromOperationAPI).Methods("POST")
+		router.HandleFunc("/replication", operation.CreateReplicantAPI).Methods(
+			"POST")
+		router.HandleFunc("/replicant/{metaID}", operation.GetReplicantAPI).Methods("GET")
+		router.HandleFunc("/replicant/{metaID}", operation.AddReplica).Methods("PUT")
+		router.HandleFunc("/replicant/{metaID}", operation.DeleteReplicant).Methods("DELETE")
+		//router.Handle("/replicants", commonHandlers.ThenFunc(operation.GetAllReplicantsAPI)).Methods("GET")
+		// Endpoint
+		router.HandleFunc("/endpoint", endpoint.CreateEndpointAPI).Methods("POST")
+		router.HandleFunc("/endpoint/createsite/{Site}/{Address}", endpoint.CreateSiteAPI).Methods("POST")
+		router.HandleFunc("/endpoint/getaddress/{Site}",
+			endpoint.GetSiteAddressAPI).Methods("GET")
+		// Database
+		// Operation
+		router.HandleFunc("/operation", operation.CreateOperationAPI).Methods("POST")
+		router.HandleFunc("/operation/execute", operation.ExecuteOperationAPI).Methods("POST")
+		router.HandleFunc("/operation/localrequest",
+			operation.ExecRequestLocallyAPI).
+			Methods("POST")
+		// Broker - Driver
+		router.HandleFunc("/scope", request.ExtractScope).Methods("GET")
+		router.HandleFunc("/scope/forward", request.RedirectRequest).Methods("POST")
+		router.HandleFunc("/Appb/{flexible:.*}", request.Appb).Methods("GET")
+		router.HandleFunc("/SendRemote", request.SendRemote).Methods("GET")
+		router.HandleFunc("/RegisterRemoteSite", request.RegisterRemoteSite).Methods("POST")
+		router.HandleFunc("/GetRemoteSite/{site}", request.GetRemoteSite).Methods("GET")
+		// Client
+		router.HandleFunc("/get", cli.GetHandler)
+		router.HandleFunc("/deploy", cli.DeployHandler)
+		// router.HandleFunc("/cross/", cli.CrossHandler)
+		// router.HandleFunc("/replica/", cli.ReplicaHandler)
+		router.HandleFunc("/sendoperation", cli.SendOperationToSites)
+	*/
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	http.HandleFunc("/", kubernetes.Proxy)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // Checks if the request has data
