9a39ac88bb5feedd16ae33bde166f7d23691e4be
Matthieu Rakotojaona <m@rako.space>
2023-06-19 19:31:32 +0200

    Add sync structure

--- api/sync.go
+++ api/sync.go
@@ -1,4 +1,40 @@
 package api
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
 func Sync(port int) {
+
+	router := mux.NewRouter()
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		method := r.Method
+		path := r.URL.EscapedPath
+
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("method=%v path=%v body=%d\n", method, path, len(body))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	err := http.ListenAndServe(":"+strconv.Itoa(port), router)
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+type request struct {
+	method string
+	path   string
+	body   []byte
 }
