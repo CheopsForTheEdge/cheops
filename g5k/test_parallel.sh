8943015f5989534d25c8016267b89936b717c584
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainamangavelo@imt-atlantique.fr>
2023-10-02 23:49:20 +0200

    test_parallel.sh: Improve readability

--- g5k/test_parallel.sh
+++ g5k/test_parallel.sh
@@ -12,7 +12,7 @@ nn3=$(printenv $n3)
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/1/ ; s/PORT/80/" ../simple-deployment.yml.tpl > simple-deployment.yml
 curl -s "http://$nn1:8079" --data-binary @simple-deployment.yml
 
-read -p "Continue ?"
+read -p "Continue ? "
 
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/2/ ; s/PORT/80/" ../simple-deployment.yml.tpl > simple-deployment-replicas.yml
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/1/ ; s/PORT/90/" ../simple-deployment.yml.tpl > simple-deployment-port.yml
