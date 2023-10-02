347c13b58ba04ffdd47b4cc819cc6f4362627759
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainamangavelo@imt-atlantique.fr>
2023-10-02 23:49:20 +0200

    test_parallel.sh: pretty-print replies

--- g5k/test_parallel.sh
+++ g5k/test_parallel.sh
@@ -10,15 +10,12 @@ n3=$(id -un)_NODE_3
 nn3=$(printenv $n3)
 
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/1/ ; s/PORT/80/" ../simple-deployment.yml.tpl > simple-deployment.yml
-curl -s "http://$nn1:8079" --data-binary @simple-deployment.yml
+curl -s "http://$nn1:8079" --data-binary @simple-deployment.yml | jq '.'
 
 read -p "Continue ? "
 
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/2/ ; s/PORT/80/" ../simple-deployment.yml.tpl > simple-deployment-replicas.yml
 sed "s/LOCATIONS/$nn1,$nn2,$nn3/ ; s/REPLICAS/1/ ; s/PORT/90/" ../simple-deployment.yml.tpl > simple-deployment-port.yml
 
-curl -s "http://$nn2:8079" --data-binary @simple-deployment-replicas.yml &
-curl -s "http://$nn3:8079" --data-binary @simple-deployment-port.yml &
-
-fg
-fg
+(curl -s "http://$nn2:8079" --data-binary @simple-deployment-replicas.yml | jq '.')  &
+(curl -s "http://$nn3:8079" --data-binary @simple-deployment-port.yml | jq '.') &
