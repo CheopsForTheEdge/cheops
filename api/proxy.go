3ae5b292b86f0ac734ca8c3858fc905fc5f08bad
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainamangavelo@imt-atlantique.fr>
2023-10-02 23:49:20 +0200

    Cleanup unused env variables

--- api/proxy.go
+++ api/proxy.go
@@ -1,92 +1,13 @@
 package api
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
 
 	"cheops.com/backends"
-	"cheops.com/env"
 )
 
-func proxyWaitBeforeWritingReply(ctx context.Context, host string, w http.ResponseWriter, method string, path string, header http.Header, body []byte) (*http.Response, error) {
-
-	u, err := url.Parse(fmt.Sprintf("http://%s/%s", host, path))
-	if err != nil {
-		return nil, err
-	}
-
-	newreq, err := http.NewRequestWithContext(ctx, method, u.String(), bytes.NewReader(body))
-
-	if err != nil {
-		http.Error(w, "can't build proxy request", http.StatusInternalServerError)
-		return nil, err
-	}
-
-	for key, vals := range header {
-		for _, val := range vals {
-			newreq.Header.Add(key, val)
-		}
-	}
-	newreq.Header.Add("X-Forwarded-For", env.Myip)
-
-	resp, err := http.DefaultClient.Do(newreq)
-	if err != nil {
-		http.Error(w, "can't send to backend", http.StatusInternalServerError)
-		log.Println(err)
-
-		// Not a blocking error
-		return nil, nil
-	}
-
-	return resp, nil
-}
-
-func proxyWriteReply(resp *http.Response, w http.ResponseWriter, host string) error {
-	defer resp.Body.Close()
-
-	respbuf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "can't read reply from backend", http.StatusInternalServerError)
-		// Not a blocking error
-		return nil
-	}
-
-	for key, vals := range resp.Header {
-		for _, val := range vals {
-			w.Header().Add(key, val)
-		}
-	}
-
-	w.WriteHeader(resp.StatusCode)
-
-	_, err = io.Copy(w, bytes.NewReader(respbuf))
-	if err != nil {
-		http.Error(w, "can't write reply", http.StatusInternalServerError)
-		log.Println(err)
-
-		// Not a blocking error
-		return nil
-	}
-
-	return nil
-
-}
-
 func proxy(ctx context.Context, host string, w http.ResponseWriter, method string, path string, header http.Header, body []byte) error {
-
-	// before
-	//	resp, err := proxyWaitBeforeWritingReply(ctx, host, w, method, path, header, body)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	return proxyWriteReply(resp, w, host)
-
 	_, _, err := backends.HandleKubernetes(ctx, method, path, header, body)
 	return err
 }
--- env/env.go
+++ env/env.go
@@ -5,15 +5,9 @@ import (
 	"os"
 )
 
-var Myip string
 var Myfqdn string
 
 func init() {
-	ip, ok := os.LookupEnv("MYIP")
-	if !ok {
-		log.Fatal("My IP must be given with the MYIP environment variable !")
-	}
-	Myip = ip
 
 	fqdn, ok := os.LookupEnv("MYFQDN")
 	if !ok {
