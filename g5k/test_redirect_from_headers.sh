8275384da0c377564b43b430eb413ff719b7c69b
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainamangavelo@imt-atlantique.fr>
2023-10-02 23:49:20 +0200

    redirect test: rename for more precision

--- g5k/test_redirect_from_headers.sh
+++ g5k/test_redirect_from_headers.sh
@@ -8,6 +8,8 @@ n2=$(id -un)_NODE_2
 nn2=$(printenv $n2)
 n3=$(id -un)_NODE_3
 nn3=$(printenv $n3)
+n4=$(id -un)_NODE_4
+nn4=$(printenv $n4)
 
 LOCATIONS="-H 'X-Cheops-Location: $nn1' -H 'X-Cheops-Location: $nn2' -H 'X-Cheops-Location: $nn3'"
 
@@ -16,7 +18,12 @@ eval "curl -s $LOCATIONS \"http://$nn1:8079\" --data-binary @simple-deployment.y
 
 read -p "Continue ? "
 
-# Test on a second site, without locations
+# Test on a site that knows nothing and with no location, should fail
 sed "s/REPLICAS/1/ ; s/PORT/90/" ../simple-deployment.yml.tpl > simple-deployment-port.yml
 
-curl -s "http://$nn2:8079" --data-binary @simple-deployment-replicas.yml | jq '.'
+curl -s "http://$nn4:8079" --data-binary @simple-deployment-port.yml
+
+read -p "Continue ? "
+
+# Test again but with location, this time should work
+eval "curl -L -s $LOCATIONS \"http://$nn4:8079\" --data-binary @simple-deployment.yml | jq '.'"
