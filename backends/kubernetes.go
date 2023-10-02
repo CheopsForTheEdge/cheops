2a1b8e5504ea7a34ab12281746a5ccc48245336d
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainamangavelo@imt-atlantique.fr>
2023-10-02 23:49:20 +0200

    Revert "Use resource id to get current config"
    This reverts commit 1460f3b9d96709c7cd17524540cf14b37ae5f88a.
    
    It is actually unneeded (we will always have the full yaml available in input) and scales badly
    (needs to iterate all resources)

--- backends/kubernetes.go
+++ backends/kubernetes.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -63,7 +62,7 @@ func SitesFor(method string, path string, headers http.Header, body []byte) ([]s
 	return locTrimmed, nil
 }
 
-// runWithStdin runs a command with an input to be passed to standard input and returns the combined output (stdout and stderr) as a slice of bytes and an error. If the length of input is 0 then the command is run as-is.
+// runWithStdin runs a command with an input to be passed to standard input and returns the combined output (stdout and stderr) as a slice of bytes and an error.
 //
 // If the command was run successfully with no error, err is null.
 // If the command was run successfully with a status code != 0, the error is a generic "failed". Note that stderr is included in the output
@@ -72,19 +71,16 @@ func runWithStdin(ctx context.Context, input []byte, args ...string) (output []b
 	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
 	command := cmd.String()
 	log.Printf("Running %s\n", command)
-
-	if len(input) > 0 {
-		stdin, err := cmd.StdinPipe()
-		if err != nil {
-			log.Printf("Couldn't get stdinpipe for [%s]: %v\n", command, err)
-			return nil, fmt.Errorf("internal error")
-		}
-
-		go func() {
-			defer stdin.Close()
-			io.Copy(stdin, bytes.NewBuffer(input))
-		}()
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Printf("Couldn't get stdinpipe for [%s]: %v\n", command, err)
+		return nil, fmt.Errorf("internal error")
 	}
+
+	go func() {
+		defer stdin.Close()
+		io.Copy(stdin, bytes.NewBuffer(input))
+	}()
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -112,48 +108,37 @@ func HandleKubernetes(ctx context.Context, method string, path string, headers h
 // The input is useful for determining the namespace and name. We let kubectl do the
 // magic itself here.
 // If anything goes wrong, CurrentConfig returns an empty json object
-func CurrentConfig(ctx context.Context, targetId string) []byte {
-	out, err := runWithStdin(ctx, nil, "kubectl", "get", "-o", "json", "-f", "-")
+func CurrentConfig(ctx context.Context, targetResource []byte) []byte {
+	out, err := runWithStdin(ctx, targetResource, "kubectl", "get", "-o", "yaml", "-f", "-")
 	if err != nil {
 		log.Printf("Error running cmd: %s\n", err)
 		return []byte("{}")
 	}
 
-	return extractCurrentConfig(out, targetId)
+	return extractCurrentConfig(out)
 }
 
-func extractCurrentConfig(b []byte, targetId string) []byte {
-	var ac allConfig
-	err := json.Unmarshal(b, &ac)
+func extractCurrentConfig(b []byte) []byte {
+	node, err := yaml.Parse(string(b))
 	if err != nil {
 		log.Printf("Couldn't parse yaml output: %v\n", err)
 		return []byte("{}")
 	}
+	docs, err := node.Pipe(yaml.Lookup("items"))
+	if err == nil {
+		elements, err := docs.Elements()
+		if err != nil {
+			log.Printf("Error parsing config: %v\n", err)
+			return []byte("{}")
 
-	for _, item := range ac.Items {
-		namespace := item.Metadata.NameMeta.Namespace
-		if namespace == "" {
-			namespace = "default"
 		}
-		id := fmt.Sprintf("%s:%s:%s", namespace, item.Kind, item.Metadata.NameMeta.Name)
-		if id == targetId {
-			lastAppliedConfiguration, ok := item.Metadata.Annotations["kubectl.kubernetes.io/last-applied-configuration"]
-			fmt.Println(lastAppliedConfiguration)
-			fmt.Println(ok)
-			if ok {
-				return []byte(lastAppliedConfiguration)
-			}
-		}
+		node = elements[0]
 	}
 
-	return []byte("{}")
-}
-
-type allConfig struct {
-	Items []itemConfig `json:"items"`
-}
-
-type itemConfig struct {
-	Kind     string          `json:"kind"`
-	Metadata yaml.ObjectMeta `json:"metadata"`
+	m := node.GetAnnotations("kubectl.kubernetes.io/last-applied-configuration")
+	config, ok := m["kubectl.kubernetes.io/last-applied-configuration"]
+	if !ok {
+		return []byte("{}")
+	}
+	return []byte(config)
 }
--- backends/kubernetes_test.go
+++ backends/kubernetes_test.go
@@ -1,8 +1,6 @@
 package backends
 
 import (
-	"log"
-	"os"
 	"testing"
 
 	jp "github.com/evanphx/json-patch"
@@ -76,16 +74,86 @@ func TestNoBody(t *testing.T) {
 }
 
 func TestConfigFor(t *testing.T) {
-	kubeReply, err := os.ReadFile("kubernetes_test_all_objects.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+	kubeReply := `apiVersion: v1
+items:
+- apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    annotations:
+      deployment.kubernetes.io/revision: "4"
+      kubectl.kubernetes.io/last-applied-configuration: |
+        {"apiVersion":"apps/v1","kind":"Deployment","metadata":{"annotations":{"locations":"dahu-8.grenoble.grid5000.fr,dahu-9.grenoble.grid5000.fr,dahu-23.grenoble.grid5000.fr"},"labels":{"app":"nginx"},"name":"nginx-deployment","namespace":"default"},"spec":{"replicas":1,"selector":{"matchLabels":{"app":"nginx"}},"template":{"metadata":{"labels":{"app":"nginx"}},"spec":{"containers":[{"image":"nginx:1.14.2","name":"nginx","ports":[{"containerPort":90}]}]}}}}
+      locations: dahu-8.grenoble.grid5000.fr,dahu-9.grenoble.grid5000.fr,dahu-23.grenoble.grid5000.fr
+    creationTimestamp: "2023-09-20T15:47:01Z"
+    generation: 6
+    labels:
+      app: nginx
+    name: nginx-deployment
+    namespace: default
+    resourceVersion: "9873"
+    uid: 1e5a2c12-718a-4497-84b0-923b4285a9e8
+  spec:
+    progressDeadlineSeconds: 600
+    replicas: 1
+    revisionHistoryLimit: 10
+    selector:
+      matchLabels:
+        app: nginx
+    strategy:
+      rollingUpdate:
+        maxSurge: 25%
+        maxUnavailable: 25%
+      type: RollingUpdate
+    template:
+      metadata:
+        creationTimestamp: null
+        labels:
+          app: nginx
+      spec:
+        containers:
+        - image: nginx:1.14.2
+          imagePullPolicy: IfNotPresent
+          name: nginx
+          ports:
+          - containerPort: 90
+            protocol: TCP
+          resources: {}
+          terminationMessagePath: /dev/termination-log
+          terminationMessagePolicy: File
+        dnsPolicy: ClusterFirst
+        restartPolicy: Always
+        schedulerName: default-scheduler
+        securityContext: {}
+        terminationGracePeriodSeconds: 30
+  status:
+    conditions:
+    - lastTransitionTime: "2023-09-20T15:47:01Z"
+      lastUpdateTime: "2023-09-20T15:47:01Z"
+      message: Deployment does not have minimum availability.
+      reason: MinimumReplicasUnavailable
+      status: "False"
+      type: Available
+    - lastTransitionTime: "2023-09-20T15:47:01Z"
+      lastUpdateTime: "2023-09-20T15:54:50Z"
+      message: ReplicaSet "nginx-deployment-ffc7dcfbb" is progressing.
+      reason: ReplicaSetUpdated
+      status: "True"
+      type: Progressing
+    observedGeneration: 6
+    replicas: 2
+    unavailableReplicas: 2
+    updatedReplicas: 1
+kind: List
+metadata:
+  resourceVersion: ""
+  selfLink: ""
+`
 
-	conf := extractCurrentConfig([]byte(kubeReply), "default:Deployment:nginx-deployment")
-
-	expectedConf := []byte(`{"apiVersion":"apps/v1","kind":"Deployment","metadata":{"annotations":{"locations":"econome-18.nantes.grid5000.fr,econome-3.nantes.grid5000.fr,econome-4.nantes.grid5000.fr"},"labels":{"app":"nginx"},"name":"nginx-deployment","namespace":"default"},"spec":{"replicas":1,"selector":{"matchLabels":{"app":"nginx"}},"template":{"metadata":{"labels":{"app":"nginx"}},"spec":{"containers":[{"image":"nginx:1.14.2","name":"nginx","ports":[{"containerPort":80}]}]}}}}`)
+	conf := extractCurrentConfig([]byte(kubeReply))
+	expectedConf := []byte(`{"apiVersion":"apps/v1","kind":"Deployment","metadata":{"annotations":{"locations":"dahu-8.grenoble.grid5000.fr,dahu-9.grenoble.grid5000.fr,dahu-23.grenoble.grid5000.fr"},"labels":{"app":"nginx"},"name":"nginx-deployment","namespace":"default"},"spec":{"replicas":1,"selector":{"matchLabels":{"app":"nginx"}},"template":{"metadata":{"labels":{"app":"nginx"}},"spec":{"containers":[{"image":"nginx:1.14.2","name":"nginx","ports":[{"containerPort":90}]}]}}}}`)
 
 	if !jp.Equal(conf, expectedConf) {
+
 		diff, _ := jp.CreateMergePatch(expectedConf, conf)
 		t.Fatalf("diff in current config, diff is %s, expected %s", diff, expectedConf)
 	}
