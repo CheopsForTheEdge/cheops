093e0e670ee2a05c725d1fce0f9e0ba454f67548
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2023-10-13 18:17:58 +0200

    multitail.sh: don't output full command line, too verbose

--- tests/g5k/multitail.sh
+++ tests/g5k/multitail.sh
@@ -7,5 +7,4 @@ while read node; do
 	command="$command -l \"ssh $node tail -F /tmp/cheops/cheops.log\""
 done < <(printenv | grep "$(id -un)_NODE" | cut -d '=' -f 2 )
 
-echo $command
 eval $command
