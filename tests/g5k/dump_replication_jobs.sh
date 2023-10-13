0561c6f1d5cfba5436eb49b5d2570f5b0891a959
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2023-10-13 18:18:09 +0200

    Add quick dump of replication jobs

---
+++ tests/g5k/dump_replication_jobs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+. ./env.sh
+
+env | grep "$(id -un)_NODE_" | cut -d '=' -f 2 | while read node
+do
+				echo $node
+				curl -s "admin:password@$node:5984/_scheduler/jobs" | jq -r '.jobs[] | .target' | xargs -I {} printf "\t{}\n"
+done
