0e3a61a2dbb9de08d3e7fd5abe5eaf33ed0ed56a
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2023-10-17 14:42:09 +0200

    test_different_sites.sh: update with operations

--- tests/g5k/test_different_sites.sh
+++ tests/g5k/test_different_sites.sh
@@ -14,11 +14,8 @@ nn4=$(printenv $n4)
 LOCATIONS_1="-H 'X-Cheops-Location: $nn1' -H 'X-Cheops-Location: $nn2' -H 'X-Cheops-Location: $nn3'"
 LOCATIONS_2="-H 'X-Cheops-Location: $nn1' -H 'X-Cheops-Location: $nn2' -H 'X-Cheops-Location: $nn4'"
 
-sed "s/REPLICAS/1/ ; s/PORT/80/" simple-deployment.yml.tpl > simple-deployment.yml
-eval "curl -s $LOCATIONS_1 \"http://$nn1:8079\" --data-binary @simple-deployment.yml | jq '.'"
+eval "curl -s $LOCATIONS_1 \"http://$nn1:8079/left\" --data-binary 'mkdir /tmp/foo; touch /tmp/foo/left' | jq '.'"
 
 read -p "Continue ? "
 
-sed "s/name: nginx-deployment/name: nginx-deployment-secondary/" simple-deployment.yml > simple-deployment-secondary.yml
-
-eval "curl -s $LOCATIONS_2 \"http://$nn1:8079\" --data-binary @simple-deployment-secondary.yml | jq '.'"
+eval "curl -s $LOCATIONS_2 \"http://$nn1:8079/right\" --data-binary 'mkdir /tmp/foo; touch /tmp/foo/right' | jq '.'"
