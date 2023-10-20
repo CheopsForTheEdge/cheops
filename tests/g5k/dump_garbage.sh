e326c7e9c09e97fe927a0e338b7bf228a109a8c3
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2023-10-20 11:04:18 +0200

    dump_garbage: use Type

--- tests/g5k/dump_garbage.sh
+++ tests/g5k/dump_garbage.sh
@@ -5,13 +5,7 @@
 env | grep "$(id -un)_NODE_" | cut -d '=' -f 2 | while read node
 do
 				echo $node
-				curl  -s -XPOST -H 'Content-Type: application/json' "$node:5984/cheops/_find" --data-binary "{\"selector\": {\"Locations\": {\"\$not\": {\"\$elemMatch\": {\"\$eq\": \"$node\"}}}}}"	\
-								| jq -r '.docs[] | ._id' | while read id; do
-								if [ -z "$id" ]
-							 	then
-												echo "\tNothing"
-								else
-												echo -e "\thttps://$node:5984/cheops/$id"
-								fi
-								done
+				curl  -s -XPOST -H 'Content-Type: application/json' "$node:5984/cheops/_find" --data-binary "{\"selector\": {\"Type\": \"DELETE\", \"Locations\": {\"\$elemMatch\": {\"\$eq\": \"$node\"}}}}"	\
+								| jq -rc '.docs[] | "id=" + .ResourceId + " rev=" + .ResourceRev' \
+								| awk '{print "\t" $0}'
 done
