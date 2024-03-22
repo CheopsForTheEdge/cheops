b4c18609aa31ff0f94d0defaff8848d2e2a021d6
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2024-03-22 12:03:56 +0100

    tests: fix synchronizations, make it self-runnable

--- tests/g5k/synchronization.py
+++ tests/g5k/synchronization.py
@@ -6,14 +6,10 @@ import requests
 def wait(hosts):
     def is_synchronized():
         for host in hosts:
-            changes = requests.get(f"http://{host}:5984/cheops/_changes")
-            current = changes.json()['last_seq']
-
             sched = requests.get(f"http://{host}:5984/_scheduler/docs", auth=("admin", "password"))
             for doc in sched.json()['docs']:
-                if 'info' in doc and doc['info'] and 'source_seq' in doc['info'] and doc['info']['source_seq']:
-                    synchronized = doc['info']['source_seq']
-                    if synchronized != current:
+                if 'info' in doc and doc['info'] and 'changes_pending' in doc['info']:
+                    if doc['info']['changes_pending'] and doc['info']['changes_pending'] > 0:
                         return False
         return True
 
@@ -22,3 +18,10 @@ def wait(hosts):
             break
         else:
             time.sleep(1)
+
+if __name__ == "__main__":
+    import os
+    path = os.path.expanduser("~/.oarnodes")
+    with open(path) as nodes:
+        hosts = [host.strip() for host in nodes.readlines()]
+        wait(hosts)
