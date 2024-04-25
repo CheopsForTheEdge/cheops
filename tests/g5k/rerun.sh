740591c94bc81bb415b6a1196cb810206c534e99
Marie Delavergne <marie.delavergne@inria.fr>
2024-04-25 16:04:00 +0200

    Remove assumption of cheops location

--- tests/g5k/rerun.sh
+++ tests/g5k/rerun.sh
@@ -2,6 +2,8 @@
 
 . ./env.sh
 
-parallel --nonall --tag --sshloginfile ~/.oarnodes --line-buffer sudo sh $HOME/repos/cheops/tests/g5k/restart.sh
+# from https://stackoverflow.com/a/246128, get the repository where the executed script is located
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+parallel --nonall --tag --sshloginfile ~/.oarnodes --line-buffer sudo sh $SCRIPT_DIR/restart.sh
 
 scp $(sed 1q ~/.oarnodes):/tmp/cheops/cli/cli $(dirname $0)/../../cli/ > /dev/null
