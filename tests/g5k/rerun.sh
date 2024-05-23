a1892cf3f41c77625ad993fd533d528ee9b1ae7e
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2024-05-23 14:50:45 +0200

    Allow repo to be anywhere locally

--- tests/g5k/rerun.sh
+++ tests/g5k/rerun.sh
@@ -2,6 +2,7 @@
 
 . ./env.sh
 
-parallel --nonall --tag --sshloginfile ~/.oarnodes --line-buffer sudo sh $HOME/repos/cheops/tests/g5k/restart.sh
+here=$(dirname $(realpath $0))
+parallel --nonall --tag --sshloginfile ~/.oarnodes --line-buffer sudo sh $here/restart.sh
 
 scp $(sed 1q ~/.oarnodes):/tmp/cheops/cli/cli $(dirname $0)/../../cli/ > /dev/null
