fa627884e37731a9eb7bf2bbd174850e0e0d624d
Matthieu Rakotojaona Rainimangavelo <matthieu.rakotojaona-rainimangavelo@imt-atlantique.fr>
2024-07-29 13:24:52 +0200

    Refactor to put common stuff together and add redis cluster

--- tests/g5k/install.py
+++ tests/g5k/install.py
@@ -1,60 +1,17 @@
 #!/usr/bin/env python
 
-import sys
-import os
-
-if any(['g5k-jupyterlab' in path for path in sys.path]):
-    print("Running on Grid'5000 notebooks, applying workaround for https://intranet.grid5000.fr/bugzilla/show_bug.cgi?id=13606")
-    print("Before:", sys.path)
-    sys.path.insert(1, os.environ['HOME'] + '/.local/lib/python3.9/site-packages')
-    print("After:", sys.path)
-
-import socket
-hostname = socket.gethostname()
-if hostname == "fnantes":
-    site = "nantes"
-    cluster = "econome"
-elif hostname == "fgrenoble":
-    site = "grenoble"
-    cluster = "dahu"
-elif hostname == "flille":
-    site = "lille"
-    cluster = "chiclet"
-else:
-    print("unknown frontend")
-    os.exit(1)
-
+import prelude
+from prelude import roles
 import enoslib as en
 
-en.init_logging()
-
-network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
-
-conf = (
-    en.G5kConf.from_settings(job_type=[], walltime="01:50:00", job_name="cheops")
-    .add_network_conf(network)
-    .add_machine(
-        roles=["cheops"],
-        cluster=cluster,
-        nodes=4,
-        primary_network=network,
-    )
-    .finalize()
-)
-
-provider = en.G5k(conf)
-
-roles, networks = provider.init()
-
-en.sync_info(roles, networks)
-
-with en.actions(roles=roles["cheops"], gather_facts=True) as p:
+with en.actions(roles=roles, gather_facts=True) as p:
+    p.apt(update_cache=True)
 
     #Couch
     p.apt_key(url="https://binaries2.erlang-solutions.com/GPG-KEY-pmanager.asc")
     p.apt_repository(repo="deb https://binaries2.erlang-solutions.com/debian {{ansible_distribution_release}}-esl-erlang-25 contrib")
-    p.apt(update_cache=True, pkg="esl-erlang", state="absent")
-    p.apt(update_cache=True, pkg="esl-erlang")
+    p.apt(pkg="esl-erlang", state="absent")
+    p.apt(pkg="esl-erlang")
 
     p.apt(pkg=["build-essential", "pkg-config", "libicu-dev", "libmozjs-78-dev"])
 
@@ -83,24 +40,6 @@ with en.actions(roles=roles["cheops"], gather_facts=True) as p:
         """,
         chdir="/tmp/couchdb/apache-couchdb-3.3.3"
     )
-
-    if False:
-        p.apt_key(url="https://couchdb.apache.org/repo/keys.asc")
-        p.apt_repository(repo="deb https://apache.jfrog.io/artifactory/couchdb-deb/ {{ ansible_distribution_release }} main")
-        p.shell(cmd="""
-        COUCHDB_PASSWORD=password
-        echo "couchdb couchdb/mode select standalone
-        couchdb couchdb/mode seen true
-        couchdb couchdb/bindaddress string 127.0.0.1
-        couchdb couchdb/bindaddress seen true
-        couchdb couchdb/cookie string elmo
-        couchdb couchdb/cookie seen true
-        couchdb couchdb/adminpass password ${COUCHDB_PASSWORD}
-        couchdb couchdb/adminpass seen true
-        couchdb couchdb/adminpass_again password ${COUCHDB_PASSWORD}
-        couchdb couchdb/adminpass_again seen true" | debconf-set-selections
-        """)
-        p.apt(pkg="couchdb")
 
     p.group(
         name="couchdb"
@@ -161,13 +100,77 @@ with en.actions(roles=roles["cheops"], gather_facts=True) as p:
 
 
 
-    # base and go
+    # base, go and redis
     p.apt(
-        update_cache=True,
-        pkg=["apt-transport-https", "ca-certificates", "curl", "gnupg", "lsb-release", "golang-1.19"]
+        pkg=["apt-transport-https", "ca-certificates", "curl", "gnupg", "lsb-release", "golang-1.19", "redis"]
     )
 
+    # redis
+    p.lineinfile(
+            path="/etc/redis/redis.conf",
+            search_string="bind 127.0.0.1",
+            line="bind 0.0.0.0 ::"
+    )
+    p.lineinfile(
+            path="/etc/redis/redis.conf",
+            search_string="# cluster-config-file",
+            line="cluster-config-file nodes.conf"
+    )
+    p.lineinfile(
+            path="/etc/redis/redis.conf",
+            search_string="# cluster-enabled",
+            line="cluster-enabled yes"
+    )
+    p.lineinfile(
+            path="/etc/redis/redis.conf",
+            search_string="appendonly no",
+            line="appendonly yes"
+    )
+    p.lineinfile(
+            path="/etc/redis/redis.conf",
+            search_string="# cluster-node-timeout",
+            line="cluster-node-timeout 5000"
+    )
+    p.systemd(
+            name="redis",
+            state="restarted"
+    )
+
+# Redis cluster
+results = en.run_command("redis-cli cluster info", roles=roles[:1])
+stdout = results[0].stdout
+if 'cluster_state:ok' not in stdout:
+    all_facts = en.gather_facts(roles=roles)
+    facts = all_facts['ok']
+    addresses = {}
+    for host in facts:
+        addresses[host] = facts[host]['ansible_default_ipv4']['address']
+
+    redis_hosts = [f"{addresses[role.alias]}:6379" for role in roles]
+    en.run_command(f"redis-cli --cluster create {' '.join(redis_hosts)} --cluster-replicas 1 --cluster-yes", roles = roles[:1])
+
+# YCSB
+with en.actions(roles=roles, gather_facts=True) as p:
+    p.file(
+        path="/opt/ycsb",
+        state="directory"
+    )
+    p.uri(
+            url="https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz",
+        dest="/tmp"
+    )
+    p.file(
+        path="/tmp/ycsb-0.17.0",
+        state="directory"
+    )
+    p.unarchive(
+        remote_src=True,
+        src="/tmp/ycsb-0.17.0.tar.gz",
+        dest="/opt/ycsb"
+    )
+    
+
 # k3s
-for role in roles["cheops"]:
+for role in roles:
     k3s = en.K3s(master=[role], agent=[])
     k3s.deploy()
---
+++ tests/g5k/tests.py
@@ -0,0 +1,51 @@
+import prelude
+from prelude import *
+import unittest
+import requests
+
+import firewall_block
+
+# Ensure firewall allows sync
+firewall_block.deactivate(roles_for_hosts)
+
+class CheopsTest(unittest.TestCase):
+    def main(self):
+        unittests.main()
+
+    def do(self, id, index, request):
+        r1 = requests.post(f"http://{hosts[index]}:8079/exec/{id}", files=request)
+        self.assertEqual(200, r1.status_code, id)
+
+    def wait_and_verify(self, id):
+        firewall_block.wait(hosts)
+
+        replies = [requests.get(f"http://{host}:5984/cheops/{id}") for host in hosts[:3]]
+        for reply in replies:
+            self.assertEqual(200, reply.status_code)
+            self.assertEqual(replies[0].json(), reply.json())
+        contents = [reply.json() for reply in replies]
+        for content in contents:
+            self.assertEqual(content['Operations'], contents[0]['Operations'])
+
+        # Make sure the replies are all ok
+        for host in hosts[:3]:
+            query = {"selector": {
+                "Type": "REPLY",
+                "Site": host,
+                "ResourceId": id
+            }}
+            r = requests.post(f"http://{host}:5984/cheops/_find", json=query, headers={"Content-Type": "application/json"})
+            for doc in r.json()['docs']:
+                self.assertEqual("OK", doc['Status'], f"status is KO {doc}")
+
+    def verify_shell(self, command):
+        # Make sure the directory has the correct content everywhere
+        with en.actions(roles=roles_for_hosts) as p:
+            p.shell(command)
+            results = p.results
+
+        contents = [content.payload['stdout'] for content in results.filter(task="shell")]
+        for content in contents[1:]:
+            self.assertEqual(contents[0], content)
+
+
